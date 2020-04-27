67ad8b03d5f639538112cd6f8ddfc2cc840f39cd
Oğuz ÖZGÜL <oguzhannet@gmail.com>
2020-04-27 18:33:40 +0300

    Initial Commit

---
+++ YuzTanima/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YuzTanima
+//
+//  Created by Oğuz Özgül on 27.04.2020.
+//  Copyright © 2020 Oğuz Özgül. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
